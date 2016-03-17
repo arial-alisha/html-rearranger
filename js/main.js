46e2f16546bedcaa194c7f3da8995bf734045e21
arial-alisha <aria0023@algonquinlive.com>
2016-03-16 21:27:29 -0400

    trying something

--- js/main.js
+++ js/main.js
@@ -2,3 +2,18 @@ var $h1 = $('h1');
 var $main = $('main');
 var $ul = $('ul');
 var $li = $('li');
+
+$ul.remove();
+$h1.html(document.write('<h1>Below ground veggies</h1>'));
+$ul.addClass('below');
+
+$h1.html(document.write('<h1>Above ground veggies</h1>'));
+$ul.addClass('above');
+
+$li.each(function () {
+  if ($(this).hasClass('below')) {
+    $belowUl.append($(this));
+  } else {
+    $aboveUl.append($(this));
+  }
+});
